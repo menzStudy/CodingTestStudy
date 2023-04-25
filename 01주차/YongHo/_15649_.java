2c3a4b51f0448e92cd55ae21342082b928c3fc51
YongHo Shin <gdtknight@naver.com>
2023-04-25 17:22:42 +0900

    1주차 문제 풀이 파일 업로드

---
+++ 01주차/YongHo/_15649_.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class _15649_ {
+  private static StringBuilder sb = new StringBuilder();
+  
+  public static void main(String[] args) throws Exception {
+    
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+    // Your code
+    StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+    
+    int N = Integer.parseInt(st.nextToken());
+    int M = Integer.parseInt(st.nextToken());
+    
+    int[] arr = new int[N];
+    
+    for (int i = 0; i < N; i++) {
+      arr[i] = i + 1;
+    }
+    
+    boolean[] visited = new boolean[N];
+    int[] selection = new int[M];
+    
+    permutation(arr, 0, M, visited, selection);
+    
+    System.out.println(sb);
+    br.close();
+  }
+  
+  public static void permutation(int[] arr, int selectedCnt, int totalCnt, boolean[] visited, int[] selection) {
+    if (selectedCnt == totalCnt) {
+      sb.append(Arrays.toString(selection).replaceAll("[\\[\\]\\,]", "")).append('\n');
+      return;
+    }
+    
+    for (int i = 0; i < arr.length; i++) {
+      if (!visited[i]) {
+        visited[i] = true;
+        selection[selectedCnt] = arr[i];
+        permutation(arr, selectedCnt + 1, totalCnt, visited, selection);
+        visited[i] = false;
+      }
+    }
+    
+  }
+  
+}
---
+++ 01주차/YongHo/_1978_.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class _1978_ {
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+    boolean[] prime = new boolean[1001];
+    
+    Arrays.fill(prime, true);
+    
+    prime[0] = false;
+    prime[1] = false;
+    
+    for (int i = 2; i <= 1000; i++) {
+      if (!prime[i]) {
+        continue;
+      }
+      
+      int cnt = 2;
+      for (int j = i * cnt; j <= 1000; j = i * ++cnt) {
+        prime[j] = false;
+      }
+    }
+    
+    int N = Integer.parseInt(br.readLine());
+    
+    StringTokenizer st = new StringTokenizer(br.readLine(),  " ");
+    
+    int answer = 0;
+    
+    while(st.hasMoreTokens()) {
+      if (prime[Integer.parseInt(st.nextToken())]) {
+        answer++;
+      }
+    }
+    
+    System.out.println(answer);
+  }
+}
