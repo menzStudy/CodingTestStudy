82a56508632605bf93fcb708faa44418e92faa71
Lee <ljs0429777@gmail.com>
2023-04-29 22:45:54 +0900

    1주차 문제 풀이 파일 업로드

---
+++ 01주차/_8393_.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.stream.IntStream;
+
+public class _8393_ {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+
+		System.out.println(IntStream.rangeClosed(1, n).sum());
+	}
+
+}
