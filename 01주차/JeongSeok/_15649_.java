0af142f8af20b116913c33ff719b5744ab99191f
Lee <ljs0429777@gmail.com>
2023-04-30 22:31:12 +0900

    1주차 문제 풀이 파일 업로드

---
+++ 01주차/JeongSeok/_15649_.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int N, M;
+	static int[] selected;
+	static int[] used;
+	static StringBuilder sb = new StringBuilder();
+
+	static void rec_func(int k) {
+		if (k == M + 1) {
+			for (int i = 1; i <= M; i++) {
+				sb.append(selected[i]).append(" ");
+			}
+			sb.append("\n");
+		} else {
+			for (int cand = 1; cand <= N; cand++) {
+				if (used[cand] == 1) {
+					continue;
+				}
+
+				selected[k] = cand;
+				used[cand] = 1;
+				rec_func(k + 1);
+				selected[k] = 0;
+				used[cand] = 0;
+
+			}
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		selected = new int[M + 1];
+		used = new int[N + 1];
+
+
+		rec_func(1);
+		System.out.println(sb.toString());
+	}
+
+}
