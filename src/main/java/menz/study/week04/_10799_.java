1073f0eb9d05b5212792271a22a0a6a0d25e870f
tlsrhksgh <sin99145@naver.com>
2023-05-15 16:48:42 +0900

    * feat: 1874번 - 스택 수열, 10799번 - 쇠막대기

---
+++ src/main/java/menz/study/week04/_10799_.java
@@ -0,0 +1,36 @@
+package menz.study.week04;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class _10799_ {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Stack<Character> stack = new Stack<>();
+        String str = br.readLine();
+        int cnt = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if(ch == '(') {
+                stack.push(ch);
+            } else if(ch == ')' && !stack.isEmpty()) {
+                stack.pop();
+            }
+
+            if (i > 0) {
+                if (str.charAt(i - 1) == '(' && str.charAt(i) ==')') {
+                    cnt += stack.size();
+                } else if (str.charAt(i - 1)==')' && str.charAt(i) ==')'){
+                    cnt++;
+                }
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
+
---
+++ src/main/java/menz/study/week04/_1874_.java
@@ -0,0 +1,40 @@
+package menz.study.week04;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class _1874_ {
+    public static void main(String[] args)  throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        Stack stack = new Stack();
+
+        int n = Integer.parseInt(br.readLine());
+        int lastValue = 0;
+
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(br.readLine());
+
+            if(lastValue < num) {
+                for (int j = lastValue + 1; j <= num; j++) {
+                    stack.push(j);
+                    sb.append("+").append("\n");
+                }
+                lastValue = num;
+            }
+
+            if(Integer.parseInt(stack.lastElement().toString()) != num) {
+                System.out.println("NO");
+                return ;
+            }
+
+            stack.pop();
+            sb.append("-").append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
+
