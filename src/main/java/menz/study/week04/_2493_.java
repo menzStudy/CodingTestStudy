5996ff76234897f93273c8a5fe2054344c7dd1f9
tlsrhksgh <sin99145@naver.com>
2023-05-16 20:05:54 +0900

    * feat: 2493 - 탑, 17298번 - 오큰수

---
+++ src/main/java/menz/study/week04/_2493_.java
@@ -0,0 +1,34 @@
+package menz.study.week04;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class _2493_ {
+    public static void main(String[] args) throws IOException {
+        Stack<Integer> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N + 1];
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 1; i <= N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = N; i >= 1; i--) {
+            while (!stack.isEmpty() && arr[stack.peek()] < arr[i]) {
+                arr[stack.pop()] = i;
+            }
+
+            stack.push(i);
+        }
+
+        while (!stack.isEmpty()) arr[stack.pop()] = 0;
+        for(int i = 1; i <= N; i++) sb.append(arr[i] + " ");
+        System.out.println(sb);
+    }
+}
