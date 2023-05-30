527e118022290bfb40b54fc59c6bae9b07695322
Lee <ljs0429777@gmail.com>
2023-05-30 21:44:20 +0900

    6주차 화요일 - 이정석 (#68)
    * feat : 11053번, 11722번 가장 긴 증가하는 부분 수열/긴 감소하는 부분 수열 풀이 추가

---
+++ src/main/java/menz/study/week06/JeongSeok/_11722_.java
@@ -0,0 +1,44 @@
+package menz.study.week06.JeongSeok;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _11722_ {
+	static int[] dp, arr;
+	static int max;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int T = Integer.parseInt(br.readLine());
+		dp = new int[T];
+		arr = new int[T];
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		for (int i = 0; i < T; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		// start
+		for (int i = 0; i < T; i++) {
+			dp[i] = 1;
+			for (int j = 0; j < i; j++) {
+				if (arr[i] < arr[j]) {
+					dp[i] = Math.max(dp[i], dp[j] + 1);
+				}
+			}
+		}
+
+		for (int i = 0; i < T; i++) {
+			if (dp[i] > max) {
+				max = dp[i];
+			}
+		}
+
+		System.out.println(max);
+
+	}
+}
