ce1e40a1c0514fdee11d131d2e91ab66460d5b1f
YongHo Shin <gdtknight@naver.com>
2023-06-20 00:25:17 +0900

    9주차 월요일 - 신용호 (#109)
    * test: 2589번 - 보물섬
    
    * solve: 2589번 - 보물섬 (50분)
    
    * test: 16234번 - 인구이동
    
    * solve: 16234번 - 인구이동 (60분)
    
    * solve: 프로그래머스 - 교점에 별 만들기

---
+++ src/main/java/menz/study/week09/YongHo/_2589_.java
@@ -0,0 +1,79 @@
+package menz.study.week09.YongHo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class _2589_ {
+
+  static final int[][] dirs = new int[][] {
+      { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 }
+  };
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+    int ROW = Integer.parseInt(st.nextToken());
+    int COL = Integer.parseInt(st.nextToken());
+
+    boolean[][] landMap = new boolean[ROW][COL];
+    boolean[][] visited = new boolean[ROW][COL];
+    int[][] distance = new int[ROW][COL];
+
+    String line;
+    for (int r = 0; r < ROW; r++) {
+      line = br.readLine();
+      for (int c = 0; c < COL; c++) {
+        if (line.charAt(c) == 'L')
+          landMap[r][c] = true;
+      }
+    }
+
+    Queue<int[]> queue = new LinkedList<>();
+
+    int maxDistance = 0;
+
+    for (int r = 0; r < ROW; r++) {
+      for (int c = 0; c < COL; c++) {
+        if (landMap[r][c]) {
+          visited = new boolean[ROW][COL];
+          distance = new int[ROW][COL];
+          visited[r][c] = true;
+          distance[r][c] = 0;
+          queue.offer(new int[] { r, c });
+
+          while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+            int curDistance = distance[cur[0]][cur[1]];
+
+            for (int[] dir : dirs) {
+              int nextR = cur[0] + dir[0];
+              int nextC = cur[1] + dir[1];
+
+              if (nextR < 0 || nextC < 0 || nextR >= ROW || nextC >= COL || !landMap[nextR][nextC]
+                  || visited[nextR][nextC])
+                continue;
+
+              visited[nextR][nextC] = true;
+              distance[nextR][nextC] = curDistance + 1;
+              queue.offer(new int[] { nextR, nextC });
+
+              if (maxDistance < distance[nextR][nextC])
+                maxDistance = distance[nextR][nextC];
+            }
+
+          }
+
+        }
+      }
+    }
+
+    System.out.println(maxDistance);
+
+    br.close();
+  }
+}
